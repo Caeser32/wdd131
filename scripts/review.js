aec120a62c9d4b1946b7e2e0dbdd8c3573a96f19
Caesar32 <btdmsiza@gmail.com>
2026-02-06 23:26:51 +0200

    completed the W05 Project: Website Planning Document

---
+++ scripts/review.js
@@ -0,0 +1,14 @@
+let reviewCount = localStorage.getItem("reviewCount");
+
+if (reviewCount === null) {
+    reviewCount = 0;
+} else {
+    reviewCount = parseInt(reviewCount);
+}
+
+reviewCount = reviewCount + 1;
+
+localStorage.setItem("reviewCount", reviewCount);
+
+const countDisplay = document.getElementById("review-count");
+countDisplay.textContent = reviewCount;
